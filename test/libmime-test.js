c6b096b65e639fabc0e3594e7db615d0a2a0ff65
zz <517669936@qq.com>
2022-05-18 14:33:30 +0800

    add test for decodeWordsWithSpace

--- test/libmime-test.js
+++ test/libmime-test.js
@@ -142,6 +142,27 @@ describe('libmime', () => {
         });
     });
 
+    describe('#decodeWordsWithSpace', () => {
+        it('should decode mime words', () => {
+            expect('Jõge-vaŽ zz Jõge-vaŽ Jõge-vaŽ Jõge-vaŽ').to.equal(
+                libmime.decodeWordsWithSpace(
+                    '=?ISO-8859-13?Q?J=F5ge-va=DE?= zz =?ISO-8859-13?Q?J=F5ge-va=DE?= =?ISO-8859-13?Q?J=F5ge-va=DE?= =?ISO-8859-13?Q?J=F5ge-va=DE?='
+                )
+            );
+            expect('Sssś Lałalalala').to.equal(libmime.decodeWordsWithSpace('=?UTF-8?B?U3NzxZsgTGHFgmFsYQ==?= =?UTF-8?B?bGFsYQ==?='));
+        });
+
+        it('should decode ascii range', () => {
+            let input1 = 'метель"  вьюга',
+                input2 = "метель' вьюга",
+                output1 = '=?UTF-8?Q?=D0=BC=D0=B5=D1=82=D0=B5=D0=BB=D1=8C=22_?= =?UTF-8?Q?=D0=B2=D1=8C=D1=8E=D0=B3=D0=B0?=',
+                output2 = "=?UTF-8?Q?=D0=BC=D0=B5=D1=82=D0=B5=D0=BB=D1=8C'?= =?UTF-8?Q?=D0=B2=D1=8C=D1=8E=D0=B3=D0=B0?=";
+
+            expect(libmime.decodeWordsWithSpace(output1)).to.equal(input1);
+            expect(libmime.decodeWordsWithSpace(output2)).to.equal(input2);
+        });
+    });
+
     describe('#buildHeaderParam', () => {
         it('should return unmodified', () => {
             expect([
